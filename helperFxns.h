0750a546ba4d7ba67fb04156c9db85b7e8d72e5d
cyan478 <cyan2@stuy.edu>
2017-01-24 00:21:27 -0500

    Update helperFxns.h

--- helperFxns.h
+++ helperFxns.h
@@ -4,9 +4,11 @@ void type_text(char *s);
 
 int inHand( int currHandLen, int currPlayerIndex, char * play, char **hand );
 
+int returnCTD(int d);
+
 char * returnPlay(char *s);
 
 int checkValidity (char *playedCard, char *currCard);
 
-//int cardImpact (char *playedCard);
+int cardImpact (char *playedCard);
 
