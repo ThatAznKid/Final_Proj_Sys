b57d6bcf266cbdb255f7596a7712c8ec15fa3319
cyan478 <cyan2@stuy.edu>
2017-01-24 00:20:58 -0500

    Update deck.h

--- deck.h
+++ deck.h
@@ -1,5 +1,5 @@
 #pragma once 
 
-char * draw(int i, int j); //draw a card!
+char * draw(int i, int j, int d); //draw a card!
 
 void drawHelper(char * retstr, int r); //SPECIFICALLY A HELPER FOR DRAW, NOTHING ELSE
