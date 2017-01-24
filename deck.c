b441fab545dad0062a6f31a4f83ad85431f199fb
cyan478 <cyan2@stuy.edu>
2017-01-24 00:20:39 -0500

    Update deck.c

--- deck.c
+++ deck.c
@@ -9,9 +9,9 @@
 
 void drawHelper (char * retstr, int r); 
 
-char * draw (int i, int j) {
+char * draw (int i, int j, int d) {
     
-    srand(time(NULL) + (10*i) + j);
+    srand(time(NULL) + (10*i) + j + d);
     int r = rand() % 108; //rand number [0,107]
     //printf ("int recieved: %d\n", r); 
     char * retstr = malloc (50);
