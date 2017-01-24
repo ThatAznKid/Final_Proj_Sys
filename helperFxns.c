b81c860743466a997c7c90c2a71472f53810430a
cyan478 <cyan2@stuy.edu>
2017-01-24 00:19:56 -0500

    updated helper

--- helperFxns.c
+++ helperFxns.c
@@ -19,27 +19,31 @@ void type_text(char *s){
 }
 
 int inHand( int currHandLen, int currPlayerIndex, char *play, char **hand ){
-	int checker = 0; //lowkey boolean : 0 = false ; 1 = true
-	int j;
+  int checker = 0; //lowkey boolean : 0 = false ; 1 = true
+  int j;
     for (j = 0; j < currHandLen; j++){
             //printf("current card in hand = %s\n", HANDS[currPlayerIndex][j]);
             //printf("curr player index: %d\n", currPlayerIndex);
-    	//printf("working\n");
-    	//printf("%s\n", hand[0]);
-    	//printf("HANDS[currPlayerIndex][j]: %s\n", hand[j]);
-    	//printf("play: %s\n", play);
-    	if (strcmp(play, hand[j]) == 0){
-         	checker = 1;
+      //printf("working\n");
+      //printf("%s\n", hand[0]);
+      //printf("HANDS[currPlayerIndex][j]: %s\n", hand[j]);
+      //printf("play: %s\n", play);
+      if (strcmp(play, hand[j]) == 0){
+          checker = 1;
             break;
-      	}
- 	}
+        }
+  }
     //printf("CHECKER : %d-----\n", checker);
- 	return checker;
+  return checker;
 }
 
 char * returnPlay(char *s){
   //printf("successful return of helper function: %s\n", s);
   return s;
+}
+
+int returnCTD(int d){ //Card To Draw
+  return d;
 }
 
 //can player put down his card based on curr card?
@@ -55,18 +59,26 @@ int checkValidity (char *playedCard, char *currCard){
       return 1;
   }
 
-  if (playedCard[0] == "W") //if wild +4 it's fine!
+  //if (strcmp(&playedCard[0], "W") == 0) //if wild +4 it's fine!
+  //  return 1;
+
+  if (strcmp(playedCard, "WILD COLOR") == 0 || strcmp(playedCard, "WILD +4") == 0)
     return 1;
 
+  //if (strcmp(&playedCard[0], &FIRSTLETTER) == 0)
+  //    return 1; //bool
   if (playedCard[0] == FIRSTLETTER)
-      return 1; //bool 
+    return 1; 
   
+  //if (strcmp(&playedCard[4], &SECLETTER) == 0)
+  //    return 1; //bool 
+
   if (playedCard[4] == SECLETTER)
-      return 1; //bool 
+    return 1;
 
   return 0; //not a valid card to put down
-
 }
+
   //Wild is weird 
   //Returns:
   /*  
@@ -81,15 +93,50 @@ int checkValidity (char *playedCard, char *currCard){
 
 
 
+
+int cardImpact (char *playedCard){
+  //char *NORMAL[10];
+  //NORMAL = {"0","1","2","3","4","5","6","7","8","9"};
+
+  char four = playedCard[4];
+  char five = playedCard[5];
+
+  int i;
+
+  //WILD
+  if (strcmp(playedCard, "WILD COLOR") == 0)
+    //if (strcmp(&four, "C") == 0)
+      return 93; //WILD COLOR trigger
+  if (strcmp(playedCard, "WILD +4") == 0)
+    //if (strcmp(&four, "+") == 0)
+      return 94; //WILD +4 trigger
+  
+  //+2/+4
+  if (strcmp(&four, "+") == 0){
+    if (strcmp(&five, "2") == 0)
+      return 2; //+2 trigger
+    if (strcmp(&five, "4") == 0)
+      return 4; //+4 trigger
+  }
+  //skip and reverse
+    if (strcmp(&four, "S") == 0)
+      return 5; //skip trigger
+    if (strcmp(&four, "R") == 0)
+      return 6; //reverse trigger
+
+  return 0; //should never happen ???
+}
+
+
 /*
 int cardImpact (char *playedCard){
-  char *NORMAL[10];
-  NORMAL = {"0","1","2","3","4","5","6","7","8","9"};
+  //char *NORMAL[10];
+  //NORMAL = {"0","1","2","3","4","5","6","7","8","9"};
 
   int i;
   //normal
   for (i=0; i < 10; i++){
-    if (playedCard[4] == NORMAL[i])
+    if (playedCard[4] == i)
       return 1; //normal card
   } 
   //WILD
@@ -98,6 +145,7 @@ int cardImpact (char *playedCard){
       return 93; //WILD COLOR trigger
     if (playedCard[4] == "+")
       return 94; //WILD +4 trigger
+  }
   //+2/+4
   if (playedCard[4] == "+"){
     if (playedCard[5] == "2")
@@ -113,6 +161,8 @@ int cardImpact (char *playedCard){
   return 0; //should never happen ???
 }
 
+*/
+/*
 if (cardImpact(PLAY) == 93){
   //prompt user to call for color. fgets. 
   type_text("\nYou put down a WILD COLOR. Indicate what color you would like\n"
@@ -155,7 +205,9 @@ if (cardImpact(PLAY) == 5){
 if (cardImpact(PLAY) == 6){
   //nothing (reverse)
 }
+*/
 
+/*
 //placed in beginning of turn, keeps track of if previous person placed +2 or +4
 if (cardToDraw != 0){
   if (cardtoDraw == 2){ //+2
